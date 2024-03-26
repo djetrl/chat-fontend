14dace42d89f75b995160c771ae990955e546498
djetrl <leshadjet@Mail.ru>
2024-03-26 15:51:57 +0700

    proxy

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import getCookie from '../utils/helpers/getCookie' ;
-axios.defaults.baseURL = window.location.origin;
+axios.defaults.baseURL = "https://217.71.129.139:4380/";
 axios.defaults.headers.common['token'] = getCookie('acsTKn')
 window.axios = axios;
 
